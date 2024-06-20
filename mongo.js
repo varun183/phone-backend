02f9204267669f908fb584387f5da015845a833a
S Varun <varunshanker1718@gmail.com>
2024-06-20 19:58:28 +0530

    Mongo database connection

---
+++ mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://varunshanker1718:${password}@cluster0.slmnkfk.mongodb.net/phoneApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log("Phonebook:");
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number} `);
+    });
+
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  console.log("invalid number of arguments");
+  mongoose.connection.close();
+}
